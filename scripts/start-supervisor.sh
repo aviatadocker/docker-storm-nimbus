3fe53911746a6ae5749112e565ed9a5623842052
rpcxdr <rpcxdr@users.noreply.github.com>
2015-05-06 13:26:03 -0600

    Update start-supervisor.sh

--- scripts/start-supervisor.sh
+++ scripts/start-supervisor.sh
@@ -1,7 +1,18 @@
-#export IP=`hostname -i`
+export IP=`hostname -i`
 #sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 #sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/nimbus/$IP/g" $STORM_HOME/conf/storm.yaml
 
 #echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+
+#sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+#sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+
+# A newline is needed before adding more text
+printf "\n" >> $STORM_HOME/conf/storm.yaml
+
+echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+echo "storm.local.dir: $STORM_HOME" >> $STORM_HOME/conf/storm.yaml
+
 
 /usr/sbin/sshd && supervisord
