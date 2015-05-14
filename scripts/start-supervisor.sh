4001bd3e3544bace030bae0eb05347dcbc555489
rpcxdr <rpcxdr@users.noreply.github.com>
2015-05-14 14:58:50 -0600

    Added auto deploy of topology

--- scripts/start-supervisor.sh
+++ scripts/start-supervisor.sh
@@ -14,5 +14,7 @@ printf "\n" >> $STORM_HOME/conf/storm.yaml
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
 echo "storm.local.dir: $STORM_HOME" >> $STORM_HOME/conf/storm.yaml
 
+# deploy the topology
+storm jar /root/topology/mystuff-discom-1.14.jar com.aviata.mystuff.discom.storm.topology.UserTopology MyStuff
 
 /usr/sbin/sshd && supervisord
